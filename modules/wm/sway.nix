deceda929d2d4be79bed87f3712e62731746c14f
Demis Balbach <db@minikn.xyz>
2025-06-02 22:43:08 +0200

    fix(sway): Set menu variable to global launcher

--- modules/wm/sway.nix
+++ modules/wm/sway.nix
@@ -94,6 +94,7 @@ in {
                 titlebar = false;
                 border = 2;
               };
+              menu = config.ordenada.globals.launcher;
               colors = with pkgs.lib.nix-rice.color;
                 let
                   background = base00;
