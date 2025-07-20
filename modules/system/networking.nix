60eb8d115096803ff44b40ed3424a2d0653cd1d9
Demis Balbach <db@minikn.xyz>
2025-07-20 12:40:13 +0200

    feat(networking): Add nameservers option; Adds nm-applet

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,21 +1,34 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 
 with pkgs.lib.ordenada;
-
-{
+let
+  inherit (lib) mkIf mkOption mkEnableOption types;
+  cfg = config.ordenada.features.networking;
+in {
   options = {
     ordenada.features.networking = {
-      enable = lib.mkEnableOption "the networking feature";
+      enable = mkEnableOption "the networking feature";
+      nameservers = mkOption {
+        description = "List of nameservers to use.";
+        type = types.listOf types.str;
+        default = [ ];
+      };
     };
   };
-  config = lib.mkIf config.ordenada.features.networking.enable {
+  config = mkIf cfg.enable {
     networking.useDHCP = false;
     networking.networkmanager.enable = true;
+    networking.nameservers = cfg.nameservers;
+
+    programs.nm-applet = {
+      enable = true;
+      indicator = true;
+    };
+    home-manager = mkHomeConfig config "networking" (user: {
+      ## Needed for nm-applet icon to show in tray.
+      home.packages = with pkgs; [ networkmanagerapplet ];
+    });
+
     users = mkHomeConfig config "networking" (user: {
       extraGroups = [ "networkmanager" ];
     });
