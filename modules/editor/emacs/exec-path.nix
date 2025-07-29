6b34993f2c9adf1486a9bd93c51a3a79d26dab2b
Demis Balbach <db@minikn.xyz>
2025-07-29 14:55:19 +0200

    feat(exec-path): Adds important env vars

--- modules/editor/emacs/exec-path.nix
+++ modules/editor/emacs/exec-path.nix
@@ -17,9 +17,22 @@ with pkgs.lib.ordenada;
     home-manager = mkHomeConfig config "emacs.exec-path" (user: {
       programs.emacs = mkElispConfig {
         name = "ordenada-exec-path";
-        config = ''
-          (eval-when-compile
-            (require 'exec-path-from-shell))
+        config = # elisp
+          ''
+            (require 'exec-path-from-shell)
+
+            (dolist (var
+            '("SSH_AUTH_SOCK"
+              "SSH_AGENT_PID"
+              "GPG_AGENT_INFO"
+              "GNUPGHOME"
+              "LANG"
+              "LC_CTYPE"
+              "NIX_SSL_CERT_FILE"
+              "NIX_PATH"
+            ))
+            (add-to-list 'exec-path-from-shell-variables var))
+
           (when (memq window-system '(mac ns x))
             (exec-path-from-shell-initialize))
           (when (daemonp)
