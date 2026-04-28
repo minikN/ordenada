6cf5c3dec906f7849e444747b2b7bbc34ffc1d2a
Demis Balbach <db@minikn.xyz>
2026-04-28 18:26:19 +0200

    feat(emacs-flymake): add showIndicators option

--- modules/editor/emacs/flymake.nix
+++ modules/editor/emacs/flymake.nix
@@ -1,18 +1,29 @@
-{ mkFeature, ordenada-lib, ... }:
+{ lib, mkFeature, ordenada-lib, ... }:
 
+let
+  inherit (lib) types mkOption;
+in
 mkFeature {
   name = [
     "emacs"
     "flymake"
   ];
+  options = {
+    showIndicators = mkOption {
+      type = types.bool;
+      description = "Whether to show the indicators in the left fringe.";
+      default = false;
+    };
+  };
   homeManager =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
       programs.emacs = ordenada-lib.mkElispConfig pkgs {
         name = "ordenada-flymake";
-        config = # elisp
+        config = with config.ordenada.features.emacs.flymake; # elisp
           ''
             (with-eval-after-load 'flymake
+              (setopt flymake-indicator-type ${if showIndicators then "'fringes" else "nil"})
               (let ((map flymake-mode-map))
                 (keymap-set map "M-n" #'flymake-goto-next-error)
                 (keymap-set map "M-p" #'flymake-goto-prev-error)))
