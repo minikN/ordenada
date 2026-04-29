7671a8a5cfa978f1abbaa910884043de4bce377a
Demis Balbach <db@minikn.xyz>
2026-04-29 09:07:46 +0200

    feat(emacs-flymake): Correct mode-line behaviour; remove overlays

--- modules/editor/emacs/flymake.nix
+++ modules/editor/emacs/flymake.nix
@@ -23,7 +23,19 @@ mkFeature {
         config = with config.ordenada.features.emacs.flymake; # elisp
           ''
             (with-eval-after-load 'flymake
-              (setopt flymake-indicator-type ${if showIndicators then "'fringes" else "nil"})
+              (add-to-list 'mode-line-misc-info `(flymake-mode (" " flymake-mode-line-counters " ")))
+              (setopt flymake-mode-line-lighter "")
+
+              ;; Removing "!" before errors
+              (defun ordenada--flymake-no-before-string (diag &rest _)
+                (when-let* ((ov (flymake--diag-overlay diag)))
+                  (overlay-put ov 'before-string nil)))
+              (advice-add 'flymake--highlight-line :after #'ordenada--flymake-no-before-string)
+
+              ${lib.optionalString (showIndicators == false) ''
+                (setopt flymake-fringe-indicator-position nil)
+                (setopt flymake-margin-indicator-position nil)
+              ''}
               (let ((map flymake-mode-map))
                 (keymap-set map "M-n" #'flymake-goto-next-error)
                 (keymap-set map "M-p" #'flymake-goto-prev-error)))
